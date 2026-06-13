ceabc05412fd91cded48269a9864d5882fdb0e3e
Solaleh Borji <solaleh.borjii@gmail.com>
2026-06-13 11:32:01 +0330

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+import library.ui.MainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new MainFrame();
+        });
+    }
+}
